ea59a54674c50b47df2d286fe4999a44339b04bc
Nathan Thomas <nwthomas@me.com>
2018-12-15 16:47:14 -0800

    Create JavaScript version of binary search

---
+++ binary-search-javascript.js
@@ -0,0 +1,22 @@
+function binarySearch(array, item) {
+  let low = 0;
+  let high = array.length - 1;
+
+  while (low <= high) {
+    let mid = low + high;
+    let guess = array[mid];
+    if (guess === item) {
+      return item;
+    } else if (guess > mid) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return "None";
+}
+
+const myList = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(myList, 3));
+console.log(binarySearch(myList, -1));
