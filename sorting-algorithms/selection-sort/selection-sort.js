ba76c5736d6c7470661a76ea953a4f14976d377e
Nathan Thomas <nwthomas@me.com>
2018-12-16 14:46:11 -0800

    Add selection-sort.js code

--- sorting-algorithms/selection-sort/selection-sort.js
+++ sorting-algorithms/selection-sort/selection-sort.js
@@ -1,2 +1,33 @@
 // Worst-case performance of O(n^2) for comparisons and O(n) for swaps
 // Best-case performance of 0(n^2) for comparisons and O(n) for swaps
+
+const swap = (items, firstIndex, secondIndex) => {
+  let temp = items[firstIndex];
+  items[firstIndex] = items[secondIndex];
+  items[secondIndex] = temp;
+};
+
+function selectionSort(items) {
+  let min;
+  for (let itemsIndex = 0; itemsIndex < items.length; itemsIndex++) {
+    // Set minimum to this position
+    min = itemsIndex;
+    for (
+      nextItemIndex = itemsIndex + 1;
+      nextItemIndex < items.length;
+      nextItemIndex++
+    ) {
+      //check the rest of the array to see if anything is smaller
+      if (items[nextItemIndex] < items[min]) {
+        min = nextItemIndex;
+      }
+    }
+    //if the minimum isn't in the position, swap it
+    if (itemsIndex != min) {
+      swap(items, itemsIndex, min);
+    }
+  }
+  return items;
+}
+
+console.log(selectionSort([3, 1, 9, 5, 8, 4]));
--- sorting-algorithms/selection-sort/selection-sort.py
+++ sorting-algorithms/selection-sort/selection-sort.py
@@ -1,2 +1,24 @@
 # Worst-case performance of O(n^2) for comparisons and O(n) for swaps
 # Best-case performance of 0(n^2) for comparisons and O(n) for swaps
+
+
+def find_smallest(arr):
+    smallest = arr[0]  # Stores the smallest value
+    smallest_index = 0  # Stores the index of the smallest value
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = 1
+    return smallest_index
+
+
+def selection_sort(arr):  # Sorts an array
+    new_arr = []
+    for i in range(len(arr)):
+        # Finds the smallest element in array and adds it to new array
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+print(selection_sort([5, 3, 6, 2, 10, 1]))
