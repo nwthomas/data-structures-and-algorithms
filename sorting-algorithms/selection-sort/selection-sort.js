7a67b25ea4bc94dd66d30971849060f558e80a4b
Nathan Thomas <nwthomas@me.com>
2018-12-22 00:30:26 -0800

    Convert selection-sort.py to selection-sort.js

--- sorting-algorithms/selection-sort/selection-sort.js
+++ sorting-algorithms/selection-sort/selection-sort.js
@@ -1,33 +1,26 @@
 // Worst-case performance of O(n^2) for comparisons and O(n) for swaps
 // Best-case performance of 0(n^2) for comparisons and O(n) for swaps
 
-const swap = (items, firstIndex, secondIndex) => {
-  let temp = items[firstIndex];
-  items[firstIndex] = items[secondIndex];
-  items[secondIndex] = temp;
-};
-
-function selectionSort(items) {
-  let min;
-  for (let itemsIndex = 0; itemsIndex < items.length; itemsIndex++) {
-    // Set minimum to this position
-    min = itemsIndex;
-    for (
-      nextItemIndex = itemsIndex + 1;
-      nextItemIndex < items.length;
-      nextItemIndex++
-    ) {
-      //check the rest of the array to see if anything is smaller
-      if (items[nextItemIndex] < items[min]) {
-        min = nextItemIndex;
-      }
-    }
-    //if the minimum isn't in the position, swap it
-    if (itemsIndex != min) {
-      swap(items, itemsIndex, min);
+function findSmallest(arr) {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
     }
   }
-  return items;
+  return smallestIndex;
 }
 
-console.log(selectionSort([3, 1, 9, 5, 8, 4]));
+function selectionSort(arr) {
+  const dupArr = arr.slice();
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let smallest = findSmallest(dupArr);
+    newArr.push(dupArr.splice([smallest], 1));
+  }
+  return newArr;
+}
+
+console.log(selectionSort([5, 3, 6, 2, 10, 1, 9, 4]));
