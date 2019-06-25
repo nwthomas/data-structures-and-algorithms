077c38b66802f85a39afbf84e445f20126638f5b
Nathan Thomas <nwthomas@me.com>
2019-06-24 22:44:12 -0700

    Add additional logging to quickSort.py

--- algorithms/sort-algorithms/quick-sort/quickSort.py
+++ algorithms/sort-algorithms/quick-sort/quickSort.py
@@ -2,7 +2,7 @@
 # Best-case performance of 0(n log n)
 
 
-def quicksort(array):
+def quickSort(array):
     if len(array) < 2:
         return array  # Base case
     else:
@@ -14,8 +14,11 @@ def quicksort(array):
         # Sub-array of all the elements great than the pivot
         greater = [i for i in array[1:] if i > pivot]
 
-        return quicksort(less) + [pivot] + quicksort(greater)
+        return quickSort(less) + [pivot] + quickSort(greater)
 
 
-# Output of [1, 2, 3, 5, 6, 7, 10, 11]
-print(quicksort([10, 5, 2, 3, 6, 11, 1, 7]))
+# Output of [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16, 17, 19, 20, 35, 47]
+print(quickSort([10, 5, 2, 3, 6, 11, 1, 7,
+                 16, 17, 19, 20, 15, 12, 8, 9, 47, 35]))
+
+print(quickSort([1, 2, 4, 3, 10, 6, 8, 5]))
