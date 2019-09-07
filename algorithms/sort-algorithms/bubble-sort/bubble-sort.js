d2eca253a29fc72172f558fe82d09fa32a798b91
Nathan Thomas <nwthomas@me.com>
2019-09-06 21:51:07 -0700

    Add in insertion sort in Python and JavaScript

--- algorithms/sort-algorithms/bubble-sort/bubble-sort.js
+++ algorithms/sort-algorithms/bubble-sort/bubble-sort.js
@@ -1,30 +1,23 @@
 // Worst case performance of O(n^2) for comparisons and O(n^2) for swaps
-// Best case performance of O(n) for comparisons and O(1) for swaps
+// Best case performance of O(n^2) for comparisons and O(1) for swaps
 
-function bubbleSort(array) {
-  let arrayLength = array.length;
-  // Outer sort loop
-  for (
-    let firstLoopPosition = 0;
-    firstLoopPosition < arrayLength;
-    firstLoopPosition++
-  ) {
-    // Inner sort loop
-    for (
-      let secondLoopPosition = 0;
-      secondLoopPosition < arrayLength - firstLoopPosition - 1;
-      secondLoopPosition++
-    ) {
-      // Checks if numbers need to be changed
-      if (array[secondLoopPosition] > array[secondLoopPosition + 1]) {
-        let tempVar = array[secondLoopPosition];
-        array[secondLoopPosition] = array[secondLoopPosition + 1];
-        array[secondLoopPosition + 1] = tempVar;
+function bubbleSort(arr) {
+  let swapped = false;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
       }
     }
+    if (!swapped) {
+      break;
+    } else {
+      swapped = false;
+    }
   }
-  return array;
+  return arr;
 }
 
-// Expect this to return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Expected return of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(bubbleSort([9, 10, 2, 4, 5, 3, 6, 8, 7, 1]));
--- algorithms/sort-algorithms/bubble-sort/bubble-sort.py
+++ algorithms/sort-algorithms/bubble-sort/bubble-sort.py
@@ -1,8 +1,27 @@
 # Worst case performance of O(n^2) for comparisons and O(n^2) for swaps
-# Best case performance of O(n) for comparisons and O(1) for swaps
+# Best case performance of O(n^2) for comparisons and O(1) for swaps
 
 
-def bubbleSort(array):
-    arrayLength = len(array)
-    for firstLoopPosition in array:
-      
+def bubble_sort(arr):
+    # Boolean flag to check if current iteration has swapped anything
+    swapped = False
+    for i in range(0, len(arr)):
+        # Loops over every time every time
+        for j in range(0, len(arr) - 1):
+            # Swap elements if j is lesser than j + 1
+            if arr[j] > arr[j + 1]:
+                # Swap elements
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                # Indicate via boolean flag that change has occured
+                swapped = True
+        # End all loops if not swaps have happend on current iteration
+        if not swapped:
+            break
+        # Else reset boolean flag
+        else:
+            swapped = False
+    return arr
+
+
+# Expected return of [1, 2, 3, 4, 4, 4, 5, 6, 7, 9]
+print(bubble_sort([2, 4, 5, 7, 1, 4, 3, 6, 9, 4]))
