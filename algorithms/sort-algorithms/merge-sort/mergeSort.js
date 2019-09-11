c6e89fc67cf441f3058173614cc30db678f4b4af
Nathan Thomas <nwthomas@me.com>
2019-09-11 12:13:44 -0700

    Add merge sort comments in JS and Python

--- algorithms/sort-algorithms/merge-sort/mergeSort.js
+++ algorithms/sort-algorithms/merge-sort/mergeSort.js
@@ -1,6 +1,8 @@
 function merge(arrA, arrB) {
+  // Defines length of both sub-lists
   const elements = arrA.length + arrB.length;
   const mergedArr = [];
+  // Control flow for different outcomes
   for (let i = 0; i < elements; i++) {
     if (!arrA.length) {
       mergedArr.push(arrB[0]);
@@ -16,13 +18,16 @@ function merge(arrA, arrB) {
       arrB = arrB.slice(1, arrB.length);
     }
   }
+  // Returns merged list at end of function call
   return mergedArr;
 }
 
 function mergeSort(arr) {
+  // eturns list if the list is length of 1
   if (arr.length <= 1) {
     return arr;
   } else {
+    // Defines a pivot and then recursively calls merge_sort while merging the two halves
     const pivot = Math.floor(arr.length / 2);
     const left = arr.slice(0, pivot);
     const right = arr.slice(pivot, arr.length);
--- algorithms/sort-algorithms/merge-sort/merge_sort.py
+++ algorithms/sort-algorithms/merge-sort/merge_sort.py
@@ -1,6 +1,8 @@
 def merge(listA, listB):
+    # Defines length of both sub-lists
     elements = len(listA) + len(listB)
     merged_list = []
+    # Control flow for different outcomes
     for i in range(elements):
         if len(listA) == 0:
             merged_list.append(listB[0])
@@ -14,14 +16,17 @@ def merge(listA, listB):
         else:
             merged_list.append(listB[0])
             listB = listB[1:]
+    # Returns merged list at end of function call
     return merged_list
 
 
 def merge_sort(list):
+    # Returns list if the list is length of 1
     if len(list) <= 1:
         return list
     else:
+        # Defines a pivot and then recursively calls merge_sort while merging the two halves
         pivot_index = len(list) // 2
         left = list[:pivot_index]
         right = list[pivot_index:]
-    return merge(merge_sort(left), merge_sort(right))
+        return merge(merge_sort(left), merge_sort(right))
