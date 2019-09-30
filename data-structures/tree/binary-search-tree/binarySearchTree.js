9a8c33a8a0e6a1f981d1ab482413c5e5e70509bd
Nathan Thomas <nwthomas@me.com>
2019-09-30 10:42:35 -0700

    Build out BST JavaScript

--- data-structures/tree/binary-search-tree/binarySearchTree.js
+++ data-structures/tree/binary-search-tree/binarySearchTree.js
@@ -17,7 +17,21 @@ class BinarySearchTree {
      * Inserts a new BST Node by traversing the BST and inserting
      * it appropriately.
      */
-    // Finish
+    let current = this;
+    let traverseNodes = true;
+    while (traverseNodes) {
+      if (current.value > value && current.left) {
+        current = current.left;
+      } else if (current.value <= value && current.right) {
+        current = current.right;
+      } else if (current.value > value && !current.left) {
+        current.left = new BinarySearchTree(value);
+        traverseNodes = false;
+      } else if (current.value <= value && !current.right) {
+        current.right = new BinarySearchTree(value);
+        traverseNodes = false;
+      }
+    }
   }
 
   contains(target) {
@@ -25,21 +39,55 @@ class BinarySearchTree {
      * Traverses the BST to check if a given target value is
      * included in it.
      */
-    // Finish
+    let current = this;
+    while (true) {
+      if (current.value === target) {
+        return true;
+      } else if (current.value > value && current.left) {
+        current = current.left;
+      } else if (current.value <= value && current.right) {
+        current = current.right;
+      } else if (current.value > value && !current.left) {
+        return false;
+      } else if (current.value <= value && !current.right) {
+        return false;
+      }
+    }
   }
 
   getMax() {
     /**
      * Traverses the BST to find the highest value in it.
      */
-    // Finish
+    let highest = this.value;
+    let current = this;
+    let traverseNodes = true;
+    while (traverseNodes) {
+      if (current.value > highest) {
+        highest = current.value;
+      }
+      if (current.right) {
+        current = current.right;
+      } else if (!current.right) {
+        traverseNodes = false;
+      }
+    }
+    return highest;
   }
 
   inOrderDFT(node) {
     /**
      * Returns all the values in order from low to high.
      */
-    // Finish
+    const values = [];
+    function dfs(n) {
+      values.push(n.value);
+      if (n.right) dfs(n.right);
+      if (n.left) dfs(n.left);
+    }
+    dfs(node);
+    values.sort();
+    return values;
   }
 }
 
