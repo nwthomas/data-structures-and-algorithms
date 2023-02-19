23769cb57faaf13c8cd7dd290ff6d73297632936
Nathan Thomas <nwthomas@me.com>
2023-02-18 22:27:39 -0800

    Finish remaining Python MBH tests

--- data_structures/heap/max_binary_heap/README.md
+++ data_structures/heap/max_binary_heap/README.md
@@ -4,15 +4,16 @@ Heaps are either a type of `tree` or an `array`/`list` data structure.
 
 ## TABLE OF CONTENTS
 
-- [Table of Contents](#table-of-contents)
-  - [Implementations](#implementations)
-  - [Type](#type)
-  - [Data Structure Description](#data-structure-description)
-  - [Space and Time Complexity](#space-and-time-complexity)
+-   [Table of Contents](#table-of-contents)
+    -   [Implementations](#implementations)
+    -   [Type](#type)
+    -   [Data Structure Description](#data-structure-description)
+    -   [Space and Time Complexity](#space-and-time-complexity)
 
 ## IMPLEMENTATION
 
-- [JavaScript](maxBinaryHeap.js)
+-   [JavaScript](maxBinaryHeap.js)
+-   [Python](max_binary_heap.py)
 
 ## TYPE
 
--- data_structures/heap/max_binary_heap/test_max_binary_heap.py
+++ data_structures/heap/max_binary_heap/test_max_binary_heap.py
@@ -38,13 +38,40 @@ class TestMaxBinaryHeap(unittest.TestCase):
         self.assertEqual(mbh.values[bubbled_up_value_index], 24)
 
     def test_removes_and_returns_first_value(self):
-        pass
+        mbh = MaxBinaryHeap()
+
+        mbh.insert(50)
+        mbh.insert(40)
+        mbh.insert(60)
+
+        removed_value = mbh.remove()
+        self.assertEqual(removed_value, 60)
 
     def test_removes_and_bubbles_down_values_on_right_side(self):
-        pass
+        mbh = MaxBinaryHeap()
+        values = [41, 33, 39, 18, 27, 12, 8]
+
+        for index, value in enumerate(values):
+            added_value_index = mbh.insert(value)
+            self.assertEqual(mbh.values, values[:index + 1])
+            self.assertEqual(mbh.values[added_value_index], value)
+
+        removed_value = mbh.remove()
+        self.assertEqual(removed_value, 41)
+        self.assertEqual(mbh.values, [39, 33, 12, 18, 27, 8])
 
     def test_removes_and_bubbles_down_values_on_left_side(self):
-        pass
+        mbh = MaxBinaryHeap()
+        values = [41, 33, 39, 18, 27, 12]
+
+        for index, value in enumerate(values):
+            added_value_index = mbh.insert(value)
+            self.assertEqual(mbh.values, values[:index + 1])
+            self.assertEqual(mbh.values[added_value_index], value)
+
+        removed_value = mbh.remove()
+        self.assertEqual(removed_value, 41)
+        self.assertEqual(mbh.values, [39, 33, 27, 18, 12])
 
 
 if __name__ == "__main__":
