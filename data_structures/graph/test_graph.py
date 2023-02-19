9f308d8f6422efb8249bc3340463a66a012dfa8a
Nathan Thomas <nwthomas@me.com>
2023-02-18 23:14:34 -0800

    Add initial tests file scaffolding

---
+++ data_structures/graph/test_graph.py
@@ -0,0 +1,8 @@
+from graph import Graph
+import unittest
+
+class TestGraph(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ data_structures/heap/priority_queue/test_priority_queue.py
@@ -0,0 +1,8 @@
+from priority_queue import PriorityQueue
+import unittest
+
+class TestPriorityQueue(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
